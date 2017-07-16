f6a908dbaf1af832940afd63cbcd50b1b383ee2e
davidtom <davidjtomczyk@gmail.com>
2017-07-16 19:08:44 -0400

    comment out seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,21 +6,21 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Domain.create(name: "streamable.com")
-Highlight.create(title: "Some Guy scores a goal for Some Team")
-Team.create(name: "Some Team")
-Player.create(name: "Some Guy")
-Country.create(name: "SOME COUNTRY")
-
-## Everything below must be copied and pasted into rails c - too lazy to set up
-#  associations above...
-domain = Domain.find(1)
-highlight = Highlight.find(1)
-team = Team.find(1)
-player = Player.find(1)
-country = Country.find(1)
-
-domain.highlights << highlight
-team.highlights << highlight
-player.highlights << highlight
-country.players << player
+# Domain.create(name: "streamable.com")
+# Highlight.create(title: "Some Guy scores a goal for Some Team")
+# Team.create(name: "Some Team")
+# Player.create(name: "Some Guy")
+# Country.create(name: "SOME COUNTRY")
+#
+# ## Everything below must be copied and pasted into rails c - too lazy to set up
+# #  associations above...
+# domain = Domain.find(1)
+# highlight = Highlight.find(1)
+# team = Team.find(1)
+# player = Player.find(1)
+# country = Country.find(1)
+#
+# domain.highlights << highlight
+# team.highlights << highlight
+# player.highlights << highlight
+# country.players << player
