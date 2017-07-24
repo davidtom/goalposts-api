f6da5409efec4064e83e4aa8e9f48b732c33e8b8
davidtom <davidjtomczyk@gmail.com>
2017-07-24 16:46:12 -0400

    enable serve assets

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,9 @@ module GoalPosts
       end if File.exists?(env_file)
     end
 
+    # Serve static assets from app/assets
+    config.serve_static_assets = true
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
