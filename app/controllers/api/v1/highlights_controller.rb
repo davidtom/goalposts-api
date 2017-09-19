49a9c514c8ea0cf09813382df91a4b8e41e35ead
davidtom <davidjtomczyk@gmail.com>
2017-09-19 19:49:08 -0400

    add route, cors, and action to delete a highlight

--- app/controllers/api/v1/highlights_controller.rb
+++ app/controllers/api/v1/highlights_controller.rb
@@ -1,6 +1,7 @@
 require 'will_paginate/array'
 
 class Api::V1::HighlightsController < Api::V1::BaseController
+  # skip_before_action :authorized, only: [:index]
   skip_before_action :authorized
 
   def index
@@ -8,4 +9,12 @@ class Api::V1::HighlightsController < Api::V1::BaseController
     paginate json: Highlight.all_reduced, per_page: 10
   end
 
+  def destroy
+    highlight = Highlight.find(params[:id])
+    if highlight.destroy
+      render json: {success: "highlight deleted successfully."}
+    else
+      render json: {error: "highlight could not be deleted."}
+    end
+  end
 end
